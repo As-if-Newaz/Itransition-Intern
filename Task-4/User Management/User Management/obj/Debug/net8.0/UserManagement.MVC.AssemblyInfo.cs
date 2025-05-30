9261d5afae07e1efb87685d9c9139993dea97a44
Sk. Md. Asif Newaz <asif.kravmaga@gmail.com>
2025-05-31 00:44:36 +0600

    minimal dashboard UI changes were done

--- Task-4/User Management/User Management/obj/Debug/net8.0/UserManagement.MVC.AssemblyInfo.cs
+++ Task-4/User Management/User Management/obj/Debug/net8.0/UserManagement.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8cda9d65e37e410a5649c37faf87aec3acb782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc1e2193124323027c85352b3cf479c081caad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task-4/User Management/UserManagement.DAL/obj/Debug/net8.0/UserManagement.DAL.AssemblyInfo.cs
+++ Task-4/User Management/UserManagement.DAL/obj/Debug/net8.0/UserManagement.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8cda9d65e37e410a5649c37faf87aec3acb782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc1e2193124323027c85352b3cf479c081caad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
