3a20e366284bdaf8dcdb5c87bb74c0295602aa2f
Sk. Md. Asif Newaz <asif.kravmaga@gmail.com>
2025-06-02 23:47:53 +0600

    Csv saving issue fixed

--- Task-5/Fake Book Generator/Fake Book Generator/obj/Debug/net8.0/Fake Book Generator.AssemblyInfo.cs
+++ Task-5/Fake Book Generator/Fake Book Generator/obj/Debug/net8.0/Fake Book Generator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fake Book Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd38907e681da0c4fdf124906dcc0b9b6ff00de8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6fe29fbf596b8414cfa303ff973e555e76f7f23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fake Book Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fake Book Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
